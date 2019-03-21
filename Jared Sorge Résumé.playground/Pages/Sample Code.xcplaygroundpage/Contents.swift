2a1d5261c5665f27ca7cc55bb1d4861f2d62b71d
Jared Sorge <jared@jsorge.net>
2019-03-20 22:52:04 -0700

    sample code tweaks

--- Jared Sorge Résumé.playground/Pages/Sample Code.xcplaygroundpage/Contents.swift
+++ Jared Sorge Résumé.playground/Pages/Sample Code.xcplaygroundpage/Contents.swift
@@ -3,42 +3,46 @@
  
  # Sample Code
  
- Here you can find some code that I wrote in 2017 to display a carousel of pageable views. The thing I really like about how it has turned out is that the API is approachable to get going, yet there are lots of customizations to get just the presentation that you want. Be sure to look at the CarouselView.swift file inside this playground's Sources folder
+ Here you can find some code that I wrote in 2017 to display a _carouselView of pageable views. The thing I really like about how it has turned out is that the API is approachable to get going, yet there are lots of customizations to get just the presentation that you want. Be sure to look at the CarouselView.swift file inside this playground's Sources folder
  */
 
 import UIKit
 import PlaygroundSupport
 
-class HeroVC: UIViewController {
+class HeroViewController: UIViewController {
+    private let _carouselView: CarouselView = {
+        let carousel = CarouselView()
+
+        // Toggle commenting out the following lines to see some of the flexibility of the carousel
+        carousel.isInifiniteScrolling = true
+        carousel.inactiveTileConfig = .dim(0.5)
+        carousel.horizontalPadding = 4.0
+
+        return carousel
+    }()
+
     //MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+
+        view.addSubview(_carouselView)
+        _carouselView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            _carouselView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            _carouselView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            _carouselView.topAnchor.constraint(equalTo: view.topAnchor),
+            _carouselView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            ])
+
+        _carouselView.tileSize = CGSize(width: 300.0, height: 300.0)
     }
     
     //MARK: - API
     func updateCarouselWithImages(_ ourHeroes: [HeroTile]) {
-        carouselView.display(ourHeroes)
+        _carouselView.display(ourHeroes)
     }
-    
-    //MARK: - Private
-    private lazy var carouselView: CarouselView = {
-        let carousel = CarouselView()
-        view.addSubview(carousel)
-        carousel.translatesAutoresizingMaskIntoConstraints = false
-        carousel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        carousel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        carousel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        carousel.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        carousel.tileSize = CGSize(width: 300.0, height: 300.0)
-        
-        // Toggle commenting out the following lines before the return to see some of the flexibility of the carousel
-        carousel.isInifiniteScrolling = true
-        carousel.inactiveTileConfig = .dim(0.5)
-        carousel.horizontalPadding = 4.0
-        
-        return carousel
-    }()
 }
 
 struct HeroTile: CarouselTile {
@@ -51,7 +55,7 @@ struct HeroTile: CarouselTile {
     }
 }
 
-let vc = HeroVC()
+let vc = HeroViewController()
 PlaygroundPage.current.liveView = vc
 
 let batman = HeroTile(heroImage: #imageLiteral(resourceName: "Batman.jpeg"))
